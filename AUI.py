cb0175215e69bcaf6fd0b3b01f1ed4aec6a9cf67
Argentina Ortega Sáinz <argentina.ortega@smail.inf.h-brs.de>
2015-08-16 19:29:01 +0200

    Evidence signal connection and evidence initialization

--- AUI.py
+++ AUI.py
@@ -10,8 +10,6 @@ import sys
 from PyQt4 import QtGui, QtCore
 from PyQt4.QtGui import (QMainWindow, QHBoxLayout, QVBoxLayout)
 
-# from PyQt4.uic import loadUi
-
 from aui.gui.views.sources import camera, lmap, gmap, pointcloud
 from aui.gui.robot.internal import battery, wifi
 from aui.gui.robot.joystick import joystick
@@ -23,8 +21,9 @@ from aui.mi import parameters
 
 import ui_aui
 
+# from PyQt4.uic import loadUi
 
-class AUI(QMainWindow,ui_aui.Ui_MainWin):
+class AUI(QMainWindow, ui_aui.Ui_MainWin):
     """
     Adaptive User Interface for TRADR project
     """
@@ -32,25 +31,23 @@ class AUI(QMainWindow,ui_aui.Ui_MainWin):
     def __init__(self, parent=None):
         super(AUI, self).__init__()
         self.sc_num = 0
+        self.evidence = {}
         self.setupUi(self)
+
         self.initUI()
 
+
     def initUI(self):
-        #self.setGeometry(0, 0, 1280, 800)
+        # self.setGeometry(0, 0, 1280, 800)
         self.setWindowTitle("Adaptive TRADR OCU")
         self.setMouseTracking(True)
 
         self.label.setVisible(False)
 
-
         '''
         Layout Definitions for the complete AUI
         '''
         # Global Layout for AUI Parameters + TRADR gui
-        #self.globalLayout = QHBoxLayout()
-        #self.setLayout(self.globalLayout)
-        #self.globalLayout.setMargin(0)
-
         self.MainLayout = QVBoxLayout()
         self.MainLayout.setObjectName("MainLayout")
         self.globalLayout.addLayout(self.MainLayout)
@@ -79,74 +76,35 @@ class AUI(QMainWindow,ui_aui.Ui_MainWin):
 
         self.MainLayout.addLayout(self.GUILayout)
 
-        #self.views = QtGui.QGroupBox("Available views", self)
-        #self.views.setMinimumSize(QtCore.QSize(550, 70))
-        #self.views.setMaximumSize(QtCore.QSize(5000, 150))
-        #self.views.setObjectName("views")
-        #self.viewsGroupLayout = QtGui.QHBoxLayout(self.views)
-        #self.viewsGroupLayout.setObjectName("viewsGroupLayout")
-
         self.views = mainviews.MainViews(self)
-        # minSize = QtCore.QSize(50, 50)
-        # maxSize = QtCore.QSize(70, 70)
-        # stretch = 0
         self.pointcloud = pointcloud.Pointcloud(self)
         self.pointcloud.setParent(self.views)
         self.views.availableViewsLayout.addWidget(self.pointcloud)
 
-        # minSize = QtCore.QSize(50, 50)
-        # maxSize = QtCore.QSize(70, 70)
-        # stretch = 0
         self.map = gmap.GlobalMap(self)
         self.map.setParent(self.views)
         self.views.availableViewsLayout.addWidget(self.map)
 
-        # minSize = QtCore.QSize(50, 50)
-        # maxSize = QtCore.QSize(70, 70)
-        # stretch = 0
         self.extra = lmap.LocalMap(self)
         self.extra.setParent(self.views)
         self.views.availableViewsLayout.addWidget(self.extra)
 
-        #self.MainViews = QtGui.QGridLayout()
-        #self.MainViews.setObjectName("MainViews")
-
-
-        # minSize = QtCore.QSize(100, 100)
-        # maxSize = QtCore.QSize(300, 300)
-        # stretch = 3
         self.Camera1 = camera.Camera(self, 1)
         self.views.tleftLayout.addWidget(self.Camera1)
         self.views.tleftLabel.setVisible(False)
 
-
-        # minSize = QtCore.QSize(100, 100)
-        # maxSize = QtCore.QSize(300, 300)
-        # stretch = 1
         self.Camera2 = camera.Camera(self, 2)
-        self.Camera2.setMinimumSize(QtCore.QSize(80,80))
+        self.Camera2.setMinimumSize(QtCore.QSize(80, 80))
         self.views.bleftLayout.addWidget(self.Camera2)
         self.views.bleftLabel.setVisible(False)
 
-
-        #self.ViewsLayout.addLayout(self.MainViews)
-        #self.ViewsLayout.addWidget(self.views)
-
         self.GUILayout.addWidget(self.views)
 
-        # minSize = QtCore.QSize(150, 150)
-        # maxSize = QtCore.QSize(200, 200)
-        # stretch = 1
         self.Screenshots = snapshot.Screenshots(self)
-
-
         self.GUILayout.addWidget(self.Screenshots)
 
-        # minSize = QtCore.QSize(80, 80)
-        # maxSize = QtCore.QSize(120, 120)
         self.battery = battery.Battery(self)
         self.wifi = wifi.Wifi(self)
-
         self.joystick = joystick.Joystick(self)
 
         self.StatusLayout.addWidget(self.battery)
@@ -157,21 +115,19 @@ class AUI(QMainWindow,ui_aui.Ui_MainWin):
         self.GUILayout.addLayout(self.StatusLayout)
 
         self.parameters = parameters.AUIParameters(self)
-        #self.globalLayout.addWidget(self.parameters)
         self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.parameters)
 
         for i in xrange(4):
             self.new_screenshot()
-
 
         '''
         Connect internal widgets
         '''
         self.mixedInitiative.AUItoggleButton.clicked[bool].connect(self.AUIupdate)
         self.AUIupdate()
-        QtCore.QObject.connect(self.parameters.wifiSlider, QtCore.SIGNAL("valueChanged(int)"), self.wifi.value.setNum)
-        QtCore.QObject.connect(self.parameters.batterySlider, QtCore.SIGNAL("valueChanged(int)"),
-                               self.battery.value.setNum)
+        # QtCore.QObject.connect(self.parameters.wifiSlider, QtCore.SIGNAL("valueChanged(int)"), self.wifi.value.setNum)
+        # QtCore.QObject.connect(self.parameters.batterySlider, QtCore.SIGNAL("valueChanged(int)"),
+                               #self.battery.value.setNum)
         QtCore.QObject.connect(self.parameters.batterySlider, QtCore.SIGNAL("valueChanged(int)"),
                                self.battery.battery.setValue)
         QtCore.QObject.connect(self.parameters.wifiSlider, QtCore.SIGNAL("valueChanged(int)"), self.wifi.wifi.setValue)
@@ -180,7 +136,8 @@ class AUI(QMainWindow,ui_aui.Ui_MainWin):
 
         self.battery.charge.clicked.connect(self.chargeBattery)
         self.wifi.repair.clicked.connect(self.repairWifi)
-        QtCore.QObject.connect(self.parameters.wifiSlider, QtCore.SIGNAL("valueChanged(int)"), self.statusBar.wifiBar.setValue)
+        QtCore.QObject.connect(self.parameters.wifiSlider, QtCore.SIGNAL("valueChanged(int)"),
+                               self.statusBar.wifiBar.setValue)
         QtCore.QObject.connect(self.parameters.batterySlider, QtCore.SIGNAL("valueChanged(int)"),
                                self.statusBar.batteryBar.setValue)
         self.Camera1.cam.inside.connect(self.parameters.insideWidget)
@@ -190,8 +147,39 @@ class AUI(QMainWindow,ui_aui.Ui_MainWin):
         self.extra.map.inside.connect(self.parameters.insideWidget)
         self.Screenshots.currentScreenshot.inside.connect(self.parameters.insideWidget)
 
-
         QtCore.QMetaObject.connectSlotsByName(self)
+
+        # Evidence connections
+        self.Camera1.cam.inside.connect(self.mixedInitiative.update_evidence)
+        self.Camera2.cam.inside.connect(self.mixedInitiative.update_evidence)
+        self.map.map.inside.connect(self.mixedInitiative.update_evidence)
+        self.pointcloud.pointcloud.inside.connect(self.mixedInitiative.update_evidence)
+        self.extra.map.inside.connect(self.mixedInitiative.update_evidence)
+        self.Screenshots.currentScreenshot.inside.connect(self.mixedInitiative.update_evidence)
+
+        self.joystick.joystick_direction.connect(self.mixedInitiative.update_evidence)
+        self.joystick.joystick_input.connect(self.mixedInitiative.update_evidence)
+        self.battery.lev.connect(self.mixedInitiative.update_evidence)
+        self.battery.vis.connect(self.mixedInitiative.update_evidence)
+        self.wifi.lev.connect(self.mixedInitiative.update_evidence)
+        self.wifi.vis.connect(self.mixedInitiative.update_evidence)
+        self.views.vis.connect(self.mixedInitiative.update_evidence)
+        self.Screenshots.vis.connect(self.mixedInitiative.update_evidence)
+        self.views.bleft.content.connect(self.mixedInitiative.update_evidence)
+        self.views.bright.content.connect(self.mixedInitiative.update_evidence)
+        self.views.tleft.content.connect(self.mixedInitiative.update_evidence)
+        self.views.tright.content.connect(self.mixedInitiative.update_evidence)
+        self.views.availableViews.av_wid.connect(self.mixedInitiative.update_evidence)
+
+        self.evidence.update({'C1':'MV', 'C2':'AV', 'PC':'AV','LM':'AV','GM':'AV'})
+        self.evidence.update({'battery_level': 'Ok', 'battery_visible':str(self.battery.batteryLevel.isChecked())})
+        self.evidence.update({'wifi_level': 'Ok', 'wifi_visible':str(self.wifi.wifiLevel.isChecked())})
+        self.evidence.update({'AV_visible':str(self.views.viewsGroup.isChecked())})
+        self.evidence.update({'AS_visible':str(self.Screenshots.extraScreenGroup.isChecked())})
+        self.evidence.update({'joystick_direction':'False'})
+        self.evidence.update({'Focus':'C1'})
+
+        self.mixedInitiative.initial_evidence(self.evidence)
 
         '''
         State machine.
@@ -201,24 +189,21 @@ class AUI(QMainWindow,ui_aui.Ui_MainWin):
         state2 = QtCore.QState(machine)
         state3 = QtCore.QState(machine)
         machine.setInitialState(state1)
-
-        #State1
-        #state1 =
-
+        # State1
+        # state1 =
 
         self.showMaximized()
 
-
+    def get_evidence(self):
+        return self.evidence
 
     def chargeBattery(self):
         self.parameters.batterySlider.setValue(100)
         self.battery.battery.setValue(100)
-        self.battery.value.setText('100')
 
     def repairWifi(self):
         self.parameters.wifiSlider.setValue(100)
         self.wifi.wifi.setValue(100)
-        self.battery.value.setText('100')
 
     def keyPressEvent(self, e):
         self.joystick.setFocus(True)
@@ -228,7 +213,7 @@ class AUI(QMainWindow,ui_aui.Ui_MainWin):
         self.parameters.contents.setEnabled(enable)
 
     def mouseMoveEvent(self, e):
-        #hoveredWidget = QApplication.widgetAt(e.globalPos())
+        # hoveredWidget = QApplication.widgetAt(e.globalPos())
         hoveredWidget = self.childAt(e.pos())
         if hoveredWidget == None:
             self.parameters.currentWidget.setText("")
@@ -249,12 +234,10 @@ class AUI(QMainWindow,ui_aui.Ui_MainWin):
         ns.chosen.connect(self.Screenshots.setScreenshot)
 
 
-
 def main():
     app = QtGui.QApplication(sys.argv)
-    main = AUI()
-
-    main.show()
+    adaptive_interface = AUI()
+    adaptive_interface.show()
 
     sys.exit(app.exec_())
 
